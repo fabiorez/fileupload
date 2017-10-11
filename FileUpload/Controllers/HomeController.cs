2996f397562a89625ad6743040715f9863403dc3
Fabio Rezende <fabio.rezende@gmail.com>
2017-10-11 00:45:55 -0300

    commit inicial

---
+++ FileUpload/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            if (postedFile != null)
+            {
+                string path = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                postedFile.SaveAs(path + Path.GetFileName(postedFile.FileName));
+                ViewBag.Message = "File uploaded successfully.";
+            }
+
+            return View();
+        }
+    }    
+}
